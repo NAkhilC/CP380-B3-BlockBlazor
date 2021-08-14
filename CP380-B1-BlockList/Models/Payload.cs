4a9154a832edd5bf363dc2a1c1fa13adc19bf559
NAkhilC <nallamothuakhil27@gmail.com>
2021-08-14 16:17:25 -0400

    Assignment 3/ web api with blazor

---
+++ CP380-B1-BlockList/Models/Payload.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CP380_B1_BlockList.Models
+{
+    public enum TransactionTypes
+    {
+        BUY, SELL, GRANT
+    }
+
+    public class Payload
+    {
+        public string user { get; set; }
+        public TransactionTypes transactionType { get; set; }
+        public int amount { get; set; }
+        public string item { get; set; }
+
+
+        public Payload(string user, TransactionTypes transactionType, int amount, string item)
+        {
+            this.user = user;
+            this.transactionType = transactionType;
+            this.amount = amount;
+            this.item = item;
+        }
+    }
+}
